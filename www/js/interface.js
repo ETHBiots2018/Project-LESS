ef60541db4a94148b6c97c9865925c661f1061a5
Michel Perez <michel.perez@gmx.ch>
2018-02-15 12:44:54 +0100

    several things in frontend changed/added, added interface.js

---
+++ www/js/interface.js
@@ -0,0 +1,33 @@
+function loggedIn(){ // returns true if the user is logged in MetaMask
+
+
+}
+
+function getProjects(){ // gets all projects running and their information and puts it in an array
+var projects=[][]; // First Bracket: # of incr. ID, second bracket: [0]: Name , [1]: founding goal, [2]: collected money, [3]: BC-address
+
+
+  return projects;
+}
+
+function getMoneyInvested(){ // gets the total money invested in all projects by the user
+  // get user key from MetaMask
+  var money=0;
+
+
+  return money;
+}
+
+function getEnergyTokens(){ // gets the total # energy tokens received in all projects by the user
+//get user key from MetaMask
+  var energyToken=0;
+
+  return energyToken;
+}
+
+function getProjectsUser(){ // gets all projects that the user invested in and puts their information in an array
+  //get user key from MetaMask
+var projects=[][]; // First Bracket: # of incr. ID, second bracket: [0]: Name , [1]: founding goal, [2]: invested money, [3]: produced energy tokens
+
+  return projects;
+}
---
+++ www/js/main.js
@@ -0,0 +1,70 @@
+window.addEventListener('load', function() {
+
+  // Check if Web3 has been injected by the browser:
+  if (typeof web3 !== 'undefined') {
+
+    // You have a web3 browser! Continue below!
+    startApp(web3);
+
+  } else {
+
+    $("#popup-no-metamask").removeClass("d-none")
+
+     }
+
+     if(loggedIn()){
+       $('#status-login').addClass('badge-success');
+       $('#status-login').removeClass('badge-danger');
+     }
+
+if($('#table-all-projects').length){
+    putProjectList('#table-all-projects');
+}
+
+
+})
+
+
+$('#popup-buy').on('click', function(e) {
+  if (e.target !== this)
+    return;
+  $("#popup-buy").toggleClass("d-none");
+
+});
+
+function putProjectList(id){
+  var projects= getProjects();
+
+    var htmlSource = '<tr>
+        <th scope="row" class="name"></th>
+        <td class="goal"></td>
+        <td>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width: 0%"></div>
+            </div>
+        </td>
+        <td><button class="btn btn-primary btn-sm" role="button" value="">Co-found it!</button></td>
+    </tr>';
+
+    var tree = $("<div>" + htmlSource + "</div>");
+
+    if(projects.length==0){
+
+    }
+
+  for(var i=0;i<projects.length;i++){
+
+    var row=tree;
+    row.find(".name").val(projects[i][0]);
+    row.find(".name").val(projects[i][1]);
+    var progress= Math.round(projects[i][2]/projects[i][1]*100);
+    row.find(".progress-bar").css("width":progress);
+    row.find("button").attr("value",projects[i][3]);
+
+    if(i>0){
+      $(id).val(row.html())
+    }
+    $(id).append(row.html());
+
+}
+}
